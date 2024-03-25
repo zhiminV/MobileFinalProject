b9903719d507b2153929e3af58ee3854e347fbc8
zhiminV <122182731+zhiminV@users.noreply.github.com>
2024-03-25 00:02:53 -0700

    add function to handleLogin to navigate login page

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -1,58 +1,63 @@
-import { Text, View, SafeAreaView } from 'react-native'
+import { Text, View, SafeAreaView,Alert} from 'react-native'
 import React from 'react'
 import colors from '../Helpers/colors';
 
-export default function SignUp() {
+export default function SignUp({navigation}) {
     const [email, setemail] = useState('');
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
-  return (
-    <SafeAreaView style={colors.container}>
-        <Text style={colors.text}>Email Address</Text>
-        <TextInput 
-        value={email} 
-        style={colors.input} 
-        placeholder="Email"
-        onChangeText={(text) => {
-            setemail(text);
-        }} 
-        />
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-            style={styles.input}
-            secureTextEntry={true}
-            placeholder="Password"
-            value={password}
-            onChangeText={(changedText) => {
-            setPassword(changedText);
-            }}
-        />
-        <Text style={styles.label}>Confirm Password</Text>
-        <TextInput
-            style={styles.input}
-            secureTextEntry={true}
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChangeText={(changedText) => {
-            setConfirmPassword(changedText);
-            }}
-        />
-        <View style={colors.buttonsContainer}>
-            <View style={colors.buttonView}>
-                <PressableButton customStyle={colors.cancle} onPressFunction={handleSignup}>
-                    <Text style={colors.buttonText}>SignUp</Text>
-                </PressableButton>
-            </View>
-            <View style={colors.buttonView}>
-                <PressableButton customStyle={colors.save} onPressFunction={handleLogin} disabled={isStartButtonDisabled}>
-                    <Text style={colors.buttonText}>Already SignUp? Login</Text>
-                </PressableButton>
-            </View>
-        </View> 
+
+    function handleLogin(){
+        navigation.navigate("Login");
+    }
+
+    return (
+        <SafeAreaView style={colors.container}>
+            <Text style={colors.text}>Email Address</Text>
+            <TextInput 
+            value={email} 
+            style={colors.input} 
+            placeholder="Email"
+            onChangeText={(text) => {
+                setemail(text);
+            }} 
+            />
+            <Text style={styles.label}>Password</Text>
+            <TextInput
+                style={styles.input}
+                secureTextEntry={true}
+                placeholder="Password"
+                value={password}
+                onChangeText={(changedText) => {
+                setPassword(changedText);
+                }}
+            />
+            <Text style={styles.label}>Confirm Password</Text>
+            <TextInput
+                style={styles.input}
+                secureTextEntry={true}
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChangeText={(changedText) => {
+                setConfirmPassword(changedText);
+                }}
+            />
+            <View style={colors.buttonsContainer}>
+                <View style={colors.buttonView}>
+                    <PressableButton customStyle={colors.cancle} onPressFunction={handleSignup}>
+                        <Text style={colors.buttonText}>SignUp</Text>
+                    </PressableButton>
+                </View>
+                <View style={colors.buttonView}>
+                    <PressableButton customStyle={colors.save} onPressFunction={handleLogin} disabled={isStartButtonDisabled}>
+                        <Text style={colors.buttonText}>Already SignUp? Login</Text>
+                    </PressableButton>
+                </View>
+            </View> 
 
 
-    </SafeAreaView>
-  )
+        </SafeAreaView>
+    )
 }
 
