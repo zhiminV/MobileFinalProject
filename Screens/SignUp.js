782dbe6524dd84b0e016a1219b4fe3509f690903
zhiminV <122182731+zhiminV@users.noreply.github.com>
2024-03-24 23:12:05 -0700

    add Password and Confirm Password input in signup screen

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -4,19 +4,41 @@ import colors from '../Helpers/colors';
 
 export default function SignUp() {
     const [email, setemail] = useState('');
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
   return (
     <SafeAreaView style={colors.container}>
         <Text style={colors.text}>Email Address</Text>
-            <TextInput 
-            value={email} 
-            style={colors.input} 
-            onChangeText={(text) => {
-                setemail(text);
-                setStartButtonDisabled(false)
+        <TextInput 
+        value={email} 
+        style={colors.input} 
+        placeholder="Email"
+        onChangeText={(text) => {
+            setemail(text);
+        }} 
+        />
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder="Password"
+            value={password}
+            onChangeText={(changedText) => {
+            setPassword(changedText);
+            }}
+        />
+        <Text style={styles.label}>Confirm Password</Text>
+        <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChangeText={(changedText) => {
+            setConfirmPassword(changedText);
+            }}
+        />
 
-            }} 
-            />
     </SafeAreaView>
   )
 }
