8f221d3568cecc66eeb591e7e06ab0191aae1959
zhiminV <122182731+zhiminV@users.noreply.github.com>
2024-03-25 22:16:21 -0700

    authentification work but the start creen can not open

--- App.js
+++ App.js
@@ -8,61 +8,73 @@ import SignUp from './Screens/SignUp';
 import Login from './Screens/Login';
 import Home from './Screens/Home';
 import TabNavigator from './Components/TabNavigator';
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "./firebase-files/firebaseSetup";
+import { useEffect, useState} from 'react';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [userLogined, setUserLogined] = useState(false);
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user) =>{
+      if(user){
+        setUserLogined(true);
+      }
+      else{
+        setUserLogined(false);
+      }
+    });
+  },[])
+
+  const AuthStack = (
+    <> 
+    <Stack.Screen name="Login" component={Login}/>
+    <Stack.Screen name="SignUp" component={SignUp}/>
+    </>
+  )
+  const AppStack = (
+    <>
+      {/* <Stack.Screen
+        options={{
+          headerShown: false,
+          headerTitle: "",
+        }} 
+        name="Start" 
+        component={Start} 
+      /> */}
+
+      <Stack.Screen 
+        options={({ route }) => {
+          return {
+            headerTitle: route.params?.tabName,
+            headerShown: false,
+            headerTitle: "",
+          };
+        }}
+        name="Home" 
+        component = {TabNavigator}
+      /> 
+    </>
+  )
+
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-            screenOptions={{
-              headerTintColor: "white",
-              headerTitleAlign: 'center',
-              headerStyle: colors.hearderColor,
-            }}
-          >
-            <Stack.Screen
-              options={{
-                headerShown: false,
-                headerTitle: "",
-              }} 
-              name="Start" 
-              component={Start} 
-            />
-            <Stack.Screen
-              options={{
-                headerShown: false,
-                headerTitle: "",
-              }} 
-              name="SignUp" 
-              component={SignUp} 
-            />
-
-            <Stack.Screen
-              options={{
-                headerShown: false,
-                headerTitle: "",
-              }} 
-              name="Login" 
-              component={Login} 
-            />     
-
-            <Stack.Screen 
-              options={({ route }) => {
-                return {
-                  headerTitle: route.params?.tabName,
-                  headerShown: false,
-                  headerTitle: "",
-                };
-              }}
-              name="Home" 
-              component = {TabNavigator}
-            />    
-
+      <Stack.Navigator
+        initialRouteName="SignUp"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#929" },
+          headerTintColor: "white",
+        }}
+      >
+        {userLogined ? AppStack : AuthStack}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
