a3474edc1d81f03a44d35a8254dd54524f416fc0
zhiminV <122182731+zhiminV@users.noreply.github.com>
2024-03-26 11:30:22 -0700

    add edit and notification button , and add functions to handle those button to navigate to edit page and notification page

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,33 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native'
 import React from 'react'
 import { auth } from '../firebase-files/firebaseSetup'
 import { Ionicons } from '@expo/vector-icons';
 import PressableButton from '../Components/PressableButton';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
+import colors from '../Helpers/colors';
 
-export default function Profile() {
+export default function Profile({navigation}) {
+  const [postsCount, setPostsCount] = useState(0);
+  const [followersCount, setFollowersCount] = useState(0);
+  const [followingCount, setFollowingCount] = useState(0);
 
-  function addImageHandle({navigation}){
-    // let user add photo 
+  useEffect(()=> {
+    //fetch user postsCount,followersCount,followingCount
+    
+  },[])
+
+  function addImageHandle(){
+    console.log("user can edit profile picture") 
   }
+  function handleEdit(){
+    console.log("navigate to edit page")
+
+  }
+  function handleNotification(){
+    console.log("navigate to notification page")
+  }
+
  
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={colors.container}>
+      <View style={styles.container}>
       <PressableButton onPressFunction={addImageHandle} style={styles.iconContainer}>
-        <Ionicons name="person-add-outline" size={50} color="black" /> 
+        <Ionicons name="person-add-outline" size={40} color="black" /> 
       </PressableButton>
+    
 
       <View style={styles.userInfo}>
         <View style={styles.stats}>
-          <Text style={styles.statsItem}>Posts: </Text>
-          <Text style={styles.statsItem}>Followers: </Text>
-          <Text style={styles.statsItem}>Following: </Text>
-        </View>
-        <Text>Email: {auth.currentUser.email}</Text>
-        <Text>UID: {auth.currentUser.uid}</Text>
+          <Text style={styles.statsItem}>Posts: {postsCount} </Text>
+          <Text style={styles.statsItem}>Fans: {followersCount} </Text>
+          <Text style={styles.statsItem}>Following: {followingCount} </Text>
+          </View >
+      </View >
       </View>
-    </View>
-  
+      <View style={colors.buttonsContainer}>
+          <View style={colors.buttonView}>
+              <PressableButton customStyle={colors.save} onPressFunction={handleEdit}>
+                  <Text style={colors.buttonText}>Edit Profile</Text>
+              </PressableButton>
+          </View>
+          <View style={colors.buttonView}>
+              <PressableButton customStyle={colors.save} onPressFunction={handleNotification}>
+                  <Text style={colors.buttonText}>Notification</Text>
+              </PressableButton>
+          </View>
+      </View>
+     
+    
+    </SafeAreaView>
+   
   )
 }
 
@@ -39,16 +71,16 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   iconContainer: {
-    marginRight: 20,
+    marginRight: 5,
   },
   userInfo: {
     flexDirection: 'column',
   },
   stats: {
     flexDirection: 'row',
-    marginBottom: 10,
+    marginBottom: 50,
   },
   statsItem: {
-    marginRight: 10,
+    marginRight: 5,
   },
 });
