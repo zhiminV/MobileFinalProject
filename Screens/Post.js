c235fda166294b95fe0098129f2599a25d2b111b
zhiminV <122182731+zhiminV@users.noreply.github.com>
2024-03-26 18:56:12 -0700

    add text input for post page so that user can add description

--- Screens/Post.js
+++ Screens/Post.js
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import colors from '../Helpers/colors';
 
 export default function Post() {
+  const [description, setDescription] = useState("");
   return (
     <View>
-      <Text>Post</Text>
+      <Text style={colors.text}>Description</Text>
+      <TextInput
+      value={description}
+      style = {[colors.input, { height: 100, width: 300,marginLeft:20}]}
+      onChange={(text)=>setDescription(text)}
+      />
+
+      
+
+
+
     </View>
   )
 }
