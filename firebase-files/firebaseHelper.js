ab9c624e0efaea0bf3bc7227beee10cd1d17b430
zhiminV <122182731+zhiminV@users.noreply.github.com>
2024-03-27 13:51:26 -0700

    add function to fetch data from user collection by id adn col name

--- firebase-files/firebaseHelper.js
+++ firebase-files/firebaseHelper.js
@@ -4,6 +4,7 @@ import {
   doc,
   deleteDoc,
   getDocs,
+  getDoc
 } from "firebase/firestore";
 import { database } from "./firebaseSetup";
 
@@ -38,5 +39,21 @@ export async function deleteFromDB(id) {
     await deleteDoc(doc(database, "goals", id));
   } catch (err) {
     console.log(err);
+  }
+}
+
+export async function fetchInfoById(col,docId) {
+  const docRef = doc(database, col, docId);
+  try {
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      return { docId: docSnap.id, ...docSnap.data() }; 
+    } else {
+      console.log("No such document!");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching document: ", error);
+    throw new Error(error);
   }
 }
