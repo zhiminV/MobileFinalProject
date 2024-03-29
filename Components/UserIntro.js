e433190423d53d4013624c55c5ab19c0d5a84bb9
zzcorczz <zzcorczz@gmail.com>
2024-03-28 21:18:05 -0700

    implemented the Following/Unfollowing feature.

--- Components/UserIntro.js
+++ Components/UserIntro.js
@@ -7,35 +7,64 @@ import { updateFromDB } from '../firebase-files/firebaseHelper';
 
 export default function UserIntro(props) {
 
-  [followed, setFollowed] = useState(false);
+  [followed, setFollowed] = useState();
+  [docID, setDocID] = useState('');
+  
+  useEffect(() => {
+    
+    async function search() {
 
-  function buttonHandler() {
-    if (pressed === true) {
-      setPressed(false);
-    } else {
-      setPressed(true);
-    }
-  }
+      try {
+        const collectionRef = collection(database, 'Users');
+        const query_follow = query(collectionRef, where('uid', '==', auth.currentUser.uid), where('following', 'array-contains', props.email));
+        const query_user = query(collectionRef, where('uid', '==', auth.currentUser.uid))
+        const querySnapshot = await getDocs(query_follow);
+        const querySnapshot2 = await getDocs(query_user);
+        querySnapshot.forEach((doc) => {
+          
+          //console.log(doc.data());
+          if (doc.data()) {
+            //setFollowed(true);
+          }
+        });
 
+        querySnapshot2.forEach((doc) => {
+          
+          //console.log(doc.id);
+          if (doc.data()) {
+            setDocID(doc.id);
+          }
+        });
+      } catch (err) {
+        console.log("This Error Happened in SearchBar.js", err);
+      }
+    };
+
+    search();
+
+  }, [followed])
+  
+  
+ /*
   async function profileButtonHandler() {
 
     try {
       const collectionRef = collection(database, 'Users');
       const query_follow = query(collectionRef, where('uid', '==', auth.currentUser.uid), where('following', 'array-contains', props.email));
       const querySnapshot = await getDocs(query_follow);
-      
       querySnapshot.forEach(async (doc) => {
-        
         console.log(doc.data());
+        console.log(typeof doc);
         if (doc.data()) {
+          console.log('xixi');
           
-          await updateDoc(doc(database, 'Users', props.docID), {
-            following: arrayRemove(props.email)})
+          //await updateDoc(doc(database, 'Users', doc.id), {
+           // following: arrayRemove(props.email)})
           setFollowed(false);
-          setDocID(doc.id);
+          //setDocID(doc.id);
         } else {
-          await updateDoc(doc(database, 'Users', props.docID), {
-            following: arrayUnion(props.email) });
+          await updateDoc(doc(database, 'Users', doc.id), {
+            following: arrayUnion(props.email)});
           setFollowed(true);
           //props.setFollowed(false);}
       }
@@ -47,17 +76,30 @@ export default function UserIntro(props) {
     }
 
   }
+  
+*/
+  async function followHandler() {
+    setFollowed(true);
+    await updateDoc(doc(database, 'Users', docID), {
+      following: arrayUnion(props.email)});
+    
+  }
 
+  async function unfollowHandler() {
+    setFollowed(false);
+    await updateDoc(doc(database, 'Users', docID), {
+      following: arrayRemove(props.email)});
+  }
 
 
   return (
     <View style={styles.introView}>
       <Text>{props.username}</Text>
       {followed ? (
-        <PressableButton customStyle={styles.confirmButtonStyle} onPressFunction={profileButtonHandler} >
+        <PressableButton customStyle={styles.confirmButtonStyle} onPressFunction={unfollowHandler} >
           <Text style={styles.textIntroStyle}>Unfollow</Text>
         </PressableButton>) : (
-        <PressableButton customStyle={styles.confirmButtonStyle} onPressFunction={profileButtonHandler} >
+        <PressableButton customStyle={styles.confirmButtonStyle} onPressFunction={followHandler} >
           <Text style={styles.textIntroStyle}>Follow</Text>
         </PressableButton>)
       }
