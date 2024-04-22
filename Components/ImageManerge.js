fce130534365f48ac5d4b2e0119d3a667d8d69ad
zhiminV <122182731+zhiminV@users.noreply.github.com>
2024-04-21 17:33:55 -0700

    improve icon size

--- Components/ImageManerge.js
+++ Components/ImageManerge.js
@@ -92,7 +92,7 @@ export default function ImageManerge({recieveImageUri}) {
 
   return (
     <View style = {styles.container}>
-      <Ionicons name="camera" size={30} color="black"onPress={takeImageHandle}/>
+      <Ionicons name="camera" size={25} color="black"onPress={takeImageHandle}style={styles.icon} />
     </View>
   )
 }
@@ -102,4 +102,7 @@ const styles = StyleSheet.create({
     marginVertical: 5,
   },
   image: { width: 100, height: 100 },
+  icon: {
+    marginRight:15,
+  },
 });
